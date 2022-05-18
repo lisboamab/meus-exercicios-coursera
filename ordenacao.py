2ca96d40066761ce1e181d3aefccc06b4d40093c
lisboamab <lisboamab@gmail.com>
2022-05-18 12:15:34 -0300

    todos os fizzbuzz e ordenacao

---
+++ ordenacao.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite o número inteiro 1: "))
+numero2 = int(input("Digite o número inteiro 2: "))
+numero3 = int(input("Digite o número inteiro 3: "))
+
+if numero1 < numero2 < numero3:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
