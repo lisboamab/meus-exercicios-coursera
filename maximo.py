4449d380a4bb22b08beaf77e4c6188d9970edb16
lisboamab <lisboamab@gmail.com>
2022-05-23 13:09:28 -0300

    função maximo

--- maximo.py
+++ maximo.py
@@ -1,12 +1,5 @@
-"""
-Exercício 1 - Máximo
-Escreva a função maximo que recebe 2 números inteiros como parâmetro e devolve o maior deles.
-
-"""
-def maximo(num1: int, num2: int):
-    if num1 > num2:
-        return num1
-    elif num1 == num2:
-        return num1
+def maximo(a, b):
+    if a >= b:
+        return a
     else:
-        return num2
+        return b
